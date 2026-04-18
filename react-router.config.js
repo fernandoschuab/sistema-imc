cfd08b56bf8cd3f7edece28d631f063ae694ced5
Fernando Schuab <fschuab@gmail.com>
2026-04-18 17:29:06 -0300

    chore: configure SPA routing with basename and update predeploy script for GitHub Pages compatibility

--- react-router.config.js
+++ react-router.config.js
@@ -1,5 +1,4 @@
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
   ssr: false,
+  basename: process.env.NODE_ENV === "production" ? "/sistema-imc/" : "/",
 };
